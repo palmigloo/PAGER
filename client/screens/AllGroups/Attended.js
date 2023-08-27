6d7dcbbebca9d8d74bf843b1e63e1e02ac9442aa
Abigail LI <palmigloo@gmail.com>
2023-08-27 14:12:03 -0700

    Update Attended.js

--- client/screens/AllGroups/Attended.js
+++ client/screens/AllGroups/Attended.js
@@ -23,16 +23,17 @@ import {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexGrow: 1,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    overflowY: 'scroll',
+    overflow: 'scroll',
   },
   textHeader: {
     fontSize: 24,
     paddingTop: 15,
     fontFamily: 'PoppinsBold',
+    color: 'white',
   },
   separation: {
     width: '90%',
@@ -44,14 +45,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     paddingTop: 15,
     fontFamily: 'PoppinsBold',
+    color: 'white',
   },
   groupName: {
-    fontSize: 22,
-    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontFamily: 'PoppinsBold',
+    color: 'white',
   },
   groupImg: {
     height: 100,
     width: 100,
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: 'white',
   },
   renderGroupContainer: {
     display: 'flex',
@@ -79,26 +85,26 @@ const Attended = ({ navigation }) => {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await getGroupsAttendedPerUser('DMuiKcBDEA0q95QHzbJq');
+      const response = await getGroupsAttendedPerUser(userId);
       setAttendedUserGroups(response); // -- GET
     }
     fetchData();
   }, []);
 
-  const [fontLoaded] = useFonts({
-    Poppins: require('../../assets/fonts/Poppins-Regular.ttf'),
-    PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
-    Bebas: require('../../assets/fonts/BebasNeue-Regular.ttf'),
-  });
+  // const [fontLoaded] = useFonts({
+  //   Poppins: require('../../assets/fonts/Poppins-Regular.ttf'),
+  //   PoppinsBold: require('../../assets/fonts/Poppins-Bold.ttf'),
+  //   Bebas: require('../../assets/fonts/BebasNeue-Regular.ttf'),
+  // });
 
-  if (!fontLoaded) {
-    return <Loading />;
-  }
+  // if (!fontLoaded) {
+  //   return <Loading />;
+  // }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.textHeader}>GROUPS</Text>
-      <View style={styles.separation} />
+      {/* <View style={styles.separation} /> */}
       <View style={{ alignSelf: 'flex-start', width: '90%', paddingLeft: 18 }}>
         <Text style={styles.featureHeader}>ATTENDED</Text>
       </View>
@@ -115,7 +121,7 @@ const Attended = ({ navigation }) => {
           // numColumns={2}
           renderItem={({ item }) => (
             // console.log('group ID :', item.id)
-            <View key={item.id} style={{ width: 350 }}>
+            <View key={Math.random()} style={{ width: 350 }}>
               <TouchableWithoutFeedback
                 onPress={() =>
                   navigation.navigate('IndividualGroupsIndex', item)
@@ -131,13 +137,15 @@ const Attended = ({ navigation }) => {
                       display: 'flex',
                       flexDirection: 'column',
                       alignItems: 'center',
+                      // borderWidth: 1,
+                      // borderColor: 'red',
                     }}
                   >
                     <Text style={styles.groupName}>{item.group_name}</Text>
                     <Text style={styles.groupName}>{item.event_name}</Text>
                   </View>
 
-                  <Icon name="chevron-right" size={30} color="#000000" />
+                  <Icon name="chevron-right" size={30} color="white" />
                 </View>
               </TouchableWithoutFeedback>
             </View>
@@ -150,23 +158,3 @@ const Attended = ({ navigation }) => {
 };
 
 export default Attended;
-
-// {/* <TouchableOpacity
-//   title="Upcoming"
-//   onPress={() => navigation.navigate('Upcoming', { name: 'Upcoming' })}
-// >
-//   <Text>Upcoming</Text>
-// </TouchableOpacity>
-
-// <TouchableOpacity
-//   title="Create"
-//   onPress={() => navigation.navigate('Create', { name: 'Create' })}
-// >
-//   <Text>Create</Text>
-// </TouchableOpacity> */}
-
-// {/* <Button
-// title="Upcoming"
-// onPress={() =>
-//   navigation.navigate('Upcoming', {name: 'Upcoming'})
-// } /> */}
